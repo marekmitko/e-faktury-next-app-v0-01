9d920cfc7faa69a3bc028a28b9efc805e9c72228
marekmitko <63197096+marekmitko@users.noreply.github.com>
2021-05-20 12:16:15 +0200

    'UserDashboard' component added, result Failed to compile

--- pages/user-dashboard/index.js
+++ pages/user-dashboard/index.js
@@ -1,14 +1,11 @@
+import {Dashboard} from './Components/UserDashboard/Dashboard.js';
 
-import {MDCTopAppBar} from '@material/top-app-bar';
-
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
-
-export default function RenderUserDashboard () {
+export default function RenderUserDashboard() {
     return(
     <>
-        <topAppBar></topAppBar>
-        <h1>Pulpit nawigacyjny</h1>
+
+        <Dashboard />
+   
     </>
     )
 }
